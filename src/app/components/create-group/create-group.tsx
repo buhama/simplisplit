b3d02355573975742bd5d47a22ce5193325ee0e2
buhama <hassan@hassanahmed.ca>
2025-05-17 11:35:20 -0400

    more fields

--- src/app/components/create-group/create-group.tsx
+++ src/app/components/create-group/create-group.tsx
@@ -13,9 +13,11 @@ import {
 import {
 	Form,
 	FormControl,
+	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
+	FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { ArrowRightIcon } from 'lucide-react';
@@ -24,10 +26,17 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Textarea } from '@/components/ui/textarea';
+import {
+	InputOTP,
+	InputOTPGroup,
+	InputOTPSlot,
+} from '@/components/ui/input-otp';
 
 const formSchema = z.object({
 	groupName: z.string().min(2).max(50),
 	groupDescription: z.string().max(50),
+	yourName: z.string().min(2).max(50),
+	passcode: z.string().length(6).optional(),
 });
 
 const CreateGroup = () => {
@@ -36,12 +45,15 @@ const CreateGroup = () => {
 		defaultValues: {
 			groupName: '',
 			groupDescription: '',
+			yourName: '',
+			passcode: '',
 		},
 	});
 
 	const onSubmit = (values: z.infer<typeof formSchema>) => {
 		console.log(values);
 	};
+
 	return (
 		<Dialog>
 			<DialogTrigger asChild>
@@ -90,6 +102,53 @@ const CreateGroup = () => {
 									</FormItem>
 								)}
 							/>
+							<FormField
+								control={form.control}
+								name='yourName'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>What should we call you?</FormLabel>
+										<FormControl>
+											<Input type='text' placeholder='John Doe' {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<div className='flex flex-col gap-4'>
+								<FormField
+									control={form.control}
+									name='passcode'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Passcode (optional)</FormLabel>
+											<FormControl>
+												<InputOTP
+													maxLength={6}
+													value={field.value || ''}
+													onChange={field.onChange}
+												>
+													<InputOTPGroup className='w-full justify-between'>
+														{Array.from({ length: 6 }).map((_, index) => (
+															<InputOTPSlot
+																key={index}
+																index={index}
+																className='w-[48px] h-[48px]'
+															/>
+														))}
+													</InputOTPGroup>
+												</InputOTP>
+											</FormControl>
+											<FormDescription className='text-sm text-muted-foreground'>
+												Your group is already private and secure. Adding a
+												passcode provides an extra layer of protection in case
+												someone accidentally shares your group link.
+											</FormDescription>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
 						</div>
 					</form>
 				</Form>
